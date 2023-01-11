37cadf94f86ab3b481d91a0e0cdcea547e6c8537
klean2050 <k.avramidis@windowslive.com>
2023-01-10 17:42:28 -0800

    adjust code to updates

--- ecg_augmentations/__init__.py
+++ ecg_augmentations/__init__.py
@@ -1,9 +1,11 @@
 from .apply import RandomApply
 from .compose import Compose, ComposeMany
+from .augmentations.neighbor import GetNeighbor
 from .augmentations.filter import HighPassFilter, LowPassFilter, HighLowPass
 from .augmentations.noise import GaussianNoise, RandWanderer
 from .augmentations.invert import Invert
 from .augmentations.crop import RandomCrop
 from .augmentations.reverse import Reverse
-from .augmentations.scale import Stretch, Compress, Scale
+from .augmentations.permute import Permute
+from .augmentations.scale import Stretch, Compress, Scale, TimeWarp
 from .augmentations.mask import PRMask, QRSMask, QTMask, RandMask
